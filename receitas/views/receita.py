8f05f5a5c37dc014602525e2ba89c3e918923720
Brendow Catarina Dellarmelin <brendowcata1@gmail.com>
2021-11-16 14:49:40 -0300

    Pagination

--- receitas/views/receita.py
+++ receitas/views/receita.py
@@ -2,13 +2,17 @@ from django.shortcuts import get_object_or_404, render, redirect
 from receitas.models import Receita
 from django.contrib.auth.models import User
 from django.contrib import auth, messages
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 
 def index(request):
 
     receitas = Receita.objects.order_by('-date_receita').filter(publicada=True)
+    paginator = Paginator(receitas, 6)
+    page = request.GET.get('page')
+    receitas_por_pagina = paginator.get_page(page)
 
     dados = {
-        'receitas' : receitas
+        'receitas' : receitas_por_pagina
     }
     return render(request,'receitas/index.html', dados)
 
@@ -18,18 +22,6 @@ def receita(request, receita_id):
         'receita':receita
     }
     return render(request,'receitas/receita.html', receita_a_exibir)
-
-def buscar(request):
-    lista_receitas = Receita.objects.order_by('-date_receita').filter(publicada=True)
-
-    if 'buscar' in request.GET:
-        nome_buscar = request.GET['buscar']
-        lista_receitas = lista_receitas.filter(nome_receita__icontains=nome_buscar)
-
-    dados = {
-        'receitas': lista_receitas
-    }
-    return render(request, 'receitas/buscar.html', dados)
 
 def cria_receita(request):
     if request.method == 'POST':
