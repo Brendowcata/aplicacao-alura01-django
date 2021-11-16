7c5bf40be5f06d31dbf306ae7c6b2d0916c6ac0d
Brendow Catarina Dellarmelin <brendowcata1@gmail.com>
2021-11-16 10:45:09 -0300

    refactoring

--- usuarios/views.py
+++ usuarios/views.py
@@ -1,3 +1,4 @@
+from os import name
 from django.shortcuts import redirect, render, get_object_or_404
 from django.contrib.auth.models import User
 from django.contrib import auth, messages
@@ -9,14 +10,20 @@ def cadastro(request):
         email = request.POST['email']
         senha = request.POST['password']
         senha2 = request.POST['password2']
-        if not nome.strip():
+        if campo_vazio(nome):
+            messages.error(request, 'O campo nome não pode ficar em branco!')
             return redirect('cadastro')
-        if not email.strip():
+        if campo_vazio(email):
+            messages.error(request, 'O campo e-mail não pode ficar em branco!')
             return redirect('cadastro')
-        if senha != senha2:
+        if senhas_nao_sao_iguais(senha, senha2):
             messages.error(request, 'As senhas não são iguais!')
             return redirect('cadastro')
         if User.objects.filter(email=email).exists():
+            messages.error(request, 'Usuário já cadastrado!')
+            return redirect('cadastro')
+        if User.objects.filter(username=nome).exists():
+            messages.error(request, 'Usuário já cadastrado!')
             return redirect('cadastro')
         user = User.objects.create_user(username=nome,email=email,password=senha)
         user.save()
@@ -29,7 +36,8 @@ def login(request):
     if request.method == 'POST':
         email = request.POST['email']
         senha = request.POST['senha']
-        if email == '' or senha == '':
+        if campo_vazio(email) or campo_vazio(senha):
+            messages.error(request, 'Os campos e-mail e senha não podem ficar em branco')
             return redirect('login')
         if User.objects.filter(email=email).exists():
             nome = User.objects.filter(email=email).values_list('username', flat=True).get()
@@ -71,4 +79,9 @@ def cria_receita(request):
         receita.save()
         return redirect('dashboard')
     return render(request, 'usuarios/cria_receita.html')
-    
+
+def campo_vazio(campo):
+    return not campo.strip()
+
+def senhas_nao_sao_iguais(senha, senha2):
+    return senha != senha2
