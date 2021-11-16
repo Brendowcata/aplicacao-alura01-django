b0a904ce33af3c0aa851a63301524338951cfc0c
Brendow Catarina Dellarmelin <brendowcata1@gmail.com>
2021-11-16 01:52:55 -0300

    Created new users

--- usuarios/views.py
+++ usuarios/views.py
@@ -1,6 +1,24 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
+from django.contrib.auth.models import User
 
 def cadastro(request):
+    if request.method == 'POST':
+        nome = request.POST['nome']
+        email = request.POST['email']
+        senha = request.POST['password']
+        senha2 = request.POST['password2']
+        if not nome.strip():
+            return redirect('cadastro')
+        if not email.strip():
+            return redirect('cadastro')
+        if senha != senha2:
+            return redirect('cadastro')
+        if User.objects.filter(email=email).exists():
+            return redirect('cadastro')
+        user = User.objects.create_user(username=nome,email=email,password=senha)
+        user.save()
+        return redirect('login')
+   
     return render(request, 'usuarios/cadastro.html')
 
 def login(request):
