a63465e81e15e2c392f1727302fc0b889e507b99
Brendow Catarina Dellarmelin <brendowcata1@gmail.com>
2021-11-16 01:23:42 -0300

    users app

--- alurareceita/urls.py
+++ alurareceita/urls.py
@@ -21,4 +21,5 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('receitas.urls')),
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    path('usuarios/', include('usuarios.urls')),
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT) #buscar caminho da imagem no banco de dados
